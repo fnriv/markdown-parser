ae35eeab1d82c3a9ba071ded3c0568f2570e03f7
fnriv <fnrivera@ucsd.edu>
2022-05-22 14:20:44 -0700

    Autoformatted code

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -11,37 +11,40 @@ public class MarkdownParse {
         ArrayList<String> toReturn = new ArrayList<>();
         // find the next [, then find the ], then find the (, then read link upto next )
         int currentIndex = 0;
-       
-        while(currentIndex < markdown.length()) {
+
+        while (currentIndex < markdown.length()) {
 
             int openBracket = markdown.indexOf("[", currentIndex);
-            if (openBracket < 0){
+            if (openBracket < 0) {
                 break;
-            }else if(openBracket > 0){
-                if (markdown.indexOf("!", currentIndex) + 1 == openBracket){
+            } else if (openBracket > 0) {
+                if (markdown.indexOf("!", currentIndex) + 1 == openBracket) {
                     break;
                 }
             }
 
             int closeBracketAndOpenParentheses = markdown.indexOf("](", openBracket);
-            if (closeBracketAndOpenParentheses < 0){break;}
+            if (closeBracketAndOpenParentheses < 0) {
+                break;
+            }
             int closeParen = markdown.indexOf(")", closeBracketAndOpenParentheses);
-            if(closeParen < 0){break;}
-            if(!markdown.substring(closeBracketAndOpenParentheses + 2, closeParen).contains(" ")){
-               toReturn.add(markdown.substring(closeBracketAndOpenParentheses + 2, closeParen));
+            if (closeParen < 0) {
+                break;
+            }
+            if (!markdown.substring(closeBracketAndOpenParentheses + 2, closeParen).contains(" ")) {
+                toReturn.add(markdown.substring(closeBracketAndOpenParentheses + 2, closeParen));
             }
             currentIndex = closeParen + 1;
-        
+
         }
         return toReturn;
     }
-
 
     public static void main(String[] args) throws IOException {
         Path fileName = Path.of(args[0]);
         String content = Files.readString(fileName);
         ArrayList<String> links = getLinks(content);
-	    System.out.println(links);
+        System.out.println(links);
         System.out.println("Done!");
         System.out.println("New change!!!!!!");
     }
